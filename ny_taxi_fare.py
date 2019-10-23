d538bdb4103523d30d2517d17d6e6a899538bc17
totanola <47486455+totanola@users.noreply.github.com>
2019-10-23 15:50:24 +0100

    data upload and fix date time

--- ny_taxi_fare.py
+++ ny_taxi_fare.py
@@ -7,8 +7,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
 
-df_train=pd.read_csv('new-york-city-taxi-fare-prediction/train.csv')
+train_data_path = 'new-york-city-taxi-fare-prediction/train.csv'
+test_data_path = 'new-york-city-taxi-fare-prediction/test.csv'
+
+df_train=pd.read_csv(train_data_path)
 
 df_train.head()
 
+len(df_train)
+
+df_train.describe()
 df_train.dtypes
+# this will take a while, because .to_datetime tries to be smart
+df_train['pickup_datetime']=pd.to_datetime(df_train['pickup_datetime'])
+
+# we create a smaller samle of the train data (10%)
+df_train_sample=df_train.sample(frac=0.1)
