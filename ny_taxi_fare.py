dc6922ced7a3088b87c4b950970b4f557e1edc10
totanola <47486455+totanola@users.noreply.github.com>
2019-11-11 13:52:32 +0000

    first bits

--- ny_taxi_fare.py
+++ ny_taxi_fare.py
@@ -7,8 +7,38 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
 
-df_train=pd.read_csv('new-york-city-taxi-fare-prediction/train.csv')
+from sklearn.linear_model import LinearRegression
+
+
+train_data_path = 'new-york-city-taxi-fare-prediction/train.csv'
+test_data_path = 'new-york-city-taxi-fare-prediction/test.csv'
+
+df_train=pd.read_csv(train_data_path, nrows=100000)
 
 df_train.head()
 
+len(df_train)
+
+df_train.describe()
 df_train.dtypes
+# this will take a while, because .to_datetime tries to be smart
+df_train['pickup_datetime'] = df_train['pickup_datetime'].str.slice(0,-4)
+df_train['pickup_datetime']=pd.to_datetime(df_train['pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+
+df_y=df_train[['fare_amount']]
+df_x=df_train.copy()
+
+# have to drop timestamp as the linear regression doesn't fit timestamps
+df_x.drop(columns=['fare_amount','key','pickup_datetime'], inplace=True)
+df_x.head()
+
+reg=LinearRegression().fit(df_x,df_y)
+
+#how well does this line fit the data?
+reg.score(df_x,df_y)
+#ans: shitly
+
+#now take the test data and take timestamp off
+df_test=pd.read_csv(test_data_path)
+df_x_test=df_test.copy()
+df_x_test.drop(columns=['fare_amount','key','pickup_datetime'], inplace=True)
